c17aa96a3e13e11df90096b7fc3c56cf5329463e
eden melaku <edenzzzmelaku@gmail.com>
2019-09-05 15:39:06 +0300

    main skeleton of world made

---
+++ World.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <array>
+#include <vector>
+using namespace std;
+
+
+class World
+{
+    private:
+    //vector<bool> includes sensor informations in the form of 0 or 1
+    // 1 - present and 0 not present 
+    /* WILL BE VISIBLE WHEN IN THE ROOM
+       pos0- (S)stinch - wumpus in adj
+       pos1- (B)breez  - pit in adj
+       pos2- (V)visited
+       WILL NOT BE VISIBLE just action
+       pos3- (G)gliter or gold in room  // wins the game
+       pos4- (P)pit    // looses the game
+       pos5- (W)wumpus  // agent is dead
+       
+       
+    */
+    vector<bool> boxes[4][4] ;
+    
+    public:
+
+    World();
+    //initializes an empty box with no position information
+    void initEmptyBox();
+    //generates random positions along with sensor information
+    void generatePosition();
+    //validates is a position is valid or not if not valid then bump
+    bool isValidPos(int i , int j);
+
+
+     
+
+    
+
+
+};
+
+
+
