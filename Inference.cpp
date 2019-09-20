3f8909ddd9b983e6f27f36ce4e8699741d3ebb22
Arisema Mezgebe Mihretu <arisema.mezgebe@gmail.com>
2019-09-20 19:00:26 +0300

    ConstructedInference class that works on top of knowledgebase

---
+++ Inference.cpp
@@ -0,0 +1,19 @@
+#include "Inference.h"
+
+
+vector<pair<int,int>> Inference::get_adjacent_rooms(pair<int, int> room)
+{
+    // todo
+}
+bool Inference::infer_wumpus()
+{
+    // todo
+}
+bool infer_pit()
+{
+    // todo
+}
+bool infer_ok()
+{
+    // todo
+}
