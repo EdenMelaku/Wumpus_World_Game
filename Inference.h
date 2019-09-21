a665110d0cc4971386be58c2adeabdd741b365cb
yonycherkos <yonycherkos@gmail.com>
2019-09-21 10:23:25 +0300

    Added ok rooms and visited  rooms functionality.

--- Inference.h
+++ Inference.h
@@ -6,20 +6,28 @@
 #include <vector>
 #include "KB.h"
 
-
 using namespace std;
+
+struct response{
+  pair<int, int> move_to = selected_room;
+  pair<int, int> shoot_to = selected_room;
+  KnowledgeBase updated_knowledge_base = updated_knowledge_base;
+};
 
 class Inference
 {
-    private:
-        KnowledgeBase current_kb;
-    public:
-      Inference();
-      ~Inference();
-      vector<pair<int,int>> get_adjacent_rooms(pair<int, int> room);
-      bool infer_wumpus();
-      bool infer_pit();
-      bool infer_ok();
-      bool infer_gold();
-};  
+private:
+    vector<pair<int, int>> adjacent_rooms;
+    vector<pair<int, int>> adjacent_ok_rooms;
+    vector<pair<int, int>> adjacent_visited_rooms;
+    pair<int, int> wumpus_room;
+    KnowledgeBase knowledge_base;
+public:
+  Inference(KnowledgeBase KnowledgeBase);
+  vector<pair<int, int>> get_adjacent_rooms(pair<int, int> current_room);
+  vector<pair<int, int>> get_adjacent_ok_rooms(pair<int, int> current_room);
+  vector<pair<int, int>> get_adjacent_visited_rooms(pair<int, int> current_room);
+
+  void inference(Knowledge room_knowledge, pair<int, int> current_room);
+};
 #endif;
