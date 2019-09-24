cd0fbf13b1de15c29e109a7f52720a41ec08c2fa
yonycherkos <yonycherkos@gmail.com>
2019-09-24 13:54:52 +0300

    adding find_possible_move function to the header files.

--- Inference.h
+++ Inference.h
@@ -8,26 +8,28 @@
 
 using namespace std;
 
-struct response{
-  pair<int, int> move_to = selected_room;
-  pair<int, int> shoot_to = selected_room;
-  KnowledgeBase updated_knowledge_base = updated_knowledge_base;
+struct Response{
+  pair<int, int> move_to;
+  pair<int, int> shoot_at;
+  map<pair<int, int>, Knowledge> updated_knowledge;
 };
 
 class Inference
 {
 private:
-    vector<pair<int, int>> adjacent_rooms;
-    vector<pair<int, int>> adjacent_ok_rooms;
-    vector<pair<int, int>> adjacent_visited_rooms;
-    pair<int, int> wumpus_room;
-    KnowledgeBase knowledge_base;
-public:
-  Inference(KnowledgeBase KnowledgeBase);
-  vector<pair<int, int>> get_adjacent_rooms(pair<int, int> current_room);
-  vector<pair<int, int>> get_adjacent_ok_rooms(pair<int, int> current_room);
-  vector<pair<int, int>> get_adjacent_visited_rooms(pair<int, int> current_room);
+  vector<pair<int, int >> adjacent_rooms;
+  vector<pair<int, int>> adjacent_ok_rooms;
+  vector<pair<int, int>> adjacent_visited_rooms;
 
-  void inference(Knowledge room_knowledge, pair<int, int> current_room);
+  pair<int, int> wumpus_room;
+  KnowledgeBase knowledge_base; // knowledge base object
+  map<pair<int, int>, Knowledge> knowledge; // the actual rooms knowledge base
+
+public:
+  Inference(KnowledgeBase Knowledge_base);
+  bool is_valid_position(int i, int j);
+  vector<pair<int, int>> get_adjacent_rooms(pair<int, int> current_room);
+  pair<int, int> find_possible_move(pair<int, int> current_room);
+  Response inference(pair<int, int> current_room);
 };
 #endif;
