97bbb692c8855c11b7b20b70afc835ca3e96e915
eden melaku <edenzzzmelaku@gmail.com>
2019-09-25 16:44:52 +0300

    function for inference is added

---
+++ actions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eden on 25/09/2019.
+//
+
+#include "actions.h"
---
+++ actions.h
@@ -0,0 +1,14 @@
+//
+// Created by eden on 25/09/2019.
+//
+
+#ifndef WUMPUS_WORLD_GAME_ACTIONS_H
+#define WUMPUS_WORLD_GAME_ACTIONS_H
+
+
+class actions {
+
+};
+
+
+#endif //WUMPUS_WORLD_GAME_ACTIONS_H
