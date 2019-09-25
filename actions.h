96d0ca36e9bf2315cf7153737e31c526fee7d675
eden melaku <edenzzzmelaku@gmail.com>
2019-09-25 16:57:05 +0300

    functions are added

--- actions.h
+++ actions.h
@@ -4,9 +4,18 @@
 
 #ifndef WUMPUS_WORLD_GAME_ACTIONS_H
 #define WUMPUS_WORLD_GAME_ACTIONS_H
-
+#include "sensor.h"
 
 class actions {
+private:
+    bool turnleft ;
+    bool turnright;
+    bool turnup;
+    bool turndown;
+    bool shoot;
+    bool move;
+public:
+     vector<Sensor> run();
 
 };
 
