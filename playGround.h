b1936bf7e0414874bb8d4922dc5b9425e12747dc
eden melaku <edenzzzmelaku@gmail.com>
2019-09-28 20:44:48 +0300

    variables and functions are added that are deleted from the other

--- playGround.h
+++ playGround.h
@@ -7,38 +7,49 @@ creating a playable environment is the main task of this module
 
 #ifndef PLAYGROUND_H
 #define PLAYGROUND_H
+
 #include <string>
 #include <vector>
-#include "Room.h"
-//#include "agent/sensor.h"
-//#include "actions.h"
-#include "World.h"
+#include "sensor.h"
+#include "actions.h"
+#include "playableWorld.h"
 using namespace std;
 
 class PlayGround
 {
 private:
-    bool is_wumpus_dead;
-    bool is_agent_dead;
-    bool is_gold_found;
+    
     pair<int, int> agentLocation;
     //vector<Sensor> infer(actions ac);
     //the current point of the player
     int point;
+    bool move;
+    bool shoot;
 
 public:
+   bool is_wumpus_dead;
+    bool is_agent_dead;
+    bool is_gold_found;
     World world;
+    actions action;
     string player;
-    vector<vector<bool> > visiblity;
+    string direction;
+    string turn;
+
+    playableWorld playableworld;
+    vector<vector<bool>> visiblity;
     PlayGround();
-    PlayGround(string player, World world);
+    PlayGround(string player, World world,playableWorld playableworld);
     int arrow = 1;
     void penalize(int value);
     void reward(int value);
+    void changeDir(string dir);
 
     //checks if the game is over(is_wumpus_dead  V  is_gold_found V  is_agent_dead) ,
     // can be called after every action(move, shoot)
-    bool is_game_over() {}
+    bool is_game_over();
     //vector<Sensor> infer(actions ac);
+    void kill_agent();
+    vector<sensor> infer();
 };
 #endif
