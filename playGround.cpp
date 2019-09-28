fb2d1ec9eb0b7f24c38374257fd19f554e918e64
eden melaku <edenzzzmelaku@gmail.com>
2019-09-28 20:45:42 +0300

    class re-implemented

--- playGround.cpp
+++ playGround.cpp
@@ -2,40 +2,102 @@
 // Created by eden on 25/09/2019.
 //
 
-#include <string>
-#include <vector>
+
+#include <set>
 #include "playGround.h"
 
+
 using namespace std;
-vector<vector<bool>> visiblity;
 
-void penalize(int value);
-void reward(int value);
+PlayGround::PlayGround()
+{
+    ;
+}PlayGround::PlayGround(string player, World world,playableWorld playableworld)
+{
+    world = world;
+    player = player;
+    playableworld =playableworld;
+    is_agent_dead = false;
+    is_wumpus_dead = false;
+    is_gold_found = false;
+    direction = "up";
+    action=actions(false,false,false,false,{0,0});
+}
 
+void PlayGround::penalize(int value)
+{
+    point = point - value;
+}
+
+void PlayGround::reward(int value)
+{
+    point = point + value;
+}
 //checks if the game is over(is_wumpus_dead  V  is_gold_found V  is_agent_dead) ,
 // can be called after every action(move, shoot)
-bool is_game_over()
+bool PlayGround::is_game_over()
 {
-    ;
+    return (is_agent_dead || is_gold_found);
 }
-PlayGround::PlayGround(){
-    ;
+void PlayGround::changeDir(string dir){
+    direction=dir;
 }
-PlayGround::PlayGround(string playern, World worldn)
+
+void PlayGround::kill_agent()
 {
-    world = worldn;
-    player = playern;
+  pair<int, int> agent_location = playableworld.get_agent_location();
+  pair<int, int> wumpus_location = world.get_wumpus_location();
+  vector<pair<int, int> > pit_locations = world.get_pit_locations();
+  set<pair<int,int> > pits(pit_locations.begin(),pit_locations.end());
+  if(agent_location == wumpus_location || pits.find(agent_location)!=pits.end()){
+    
+    is_agent_dead=true;
+
+  }
 }
-//kills the agent
-/*
-vector<Sensor> playGround::infer(actions ac)
+
+vector<sensor> PlayGround::infer()
 {
-    vector<Sensor> perception = ac.run();
+    pair<int, int> location=playableworld.get_agent_location();
+    if(action.turnleft) {playableworld.turn_left();}
+    else if(action.turnright) { playableworld.turn_right(); }
+    
+
+    if(move) { location = playableworld.move_forward(); }
+    if(shoot) { playableworld.shoot(); }
+
+    Room room = world.boxes.at(location.first).at(location.second);
+    vector<sensor> perception;
+    if(room.get_breeze()) {
+        Type type=Type::B;
+        sensor s=sensor(location,type);
+        perception.push_back(s);
+    }
+    if(room.get_stench()){
+        Type type=Type::S;
+        sensor s=sensor(location,type);
+        perception.push_back(s);
+    }
+    if(room.get_glitter()){
+        Type type=Type::G;
+        sensor s=sensor(location,type);
+        perception.push_back(s);
+    }
+    if(room.has_pit()){
+        Type type=Type::P;
+        sensor s=sensor(location,type);
+        perception.push_back(s);
+    }
+    if(room.has_wumpus()){
+        Type type=Type::W;
+        sensor s=sensor(location,type);
+        perception.push_back(s);
+    }
     if (is_wumpus_dead)
     {
         Type t = Type::scream;
-        Sensor s = Sensor(perception.at(0).getLocation(), t);
+        sensor s = sensor(perception.at(0).get_Location(), t);
     }
     return perception;
 }
-*/
+
