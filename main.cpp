2d9e953daa4fd318784e49d2011b10f4416ad61b
eden melaku <edenzzzmelaku@gmail.com>
2019-09-29 10:26:16 +0300

    integration error with UI and playableWorld are solved

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+/*
+g++ --std=c++11 -o sample3 Room.cpp World.cpp playableWorld.cpp sensor.cpp actions.cpp playGround.cpp main.cpp -lncurses
+
+*/
 #include "playGround.h"
 #include <iostream>
 #include <curses.h>
@@ -17,8 +21,11 @@ void printsep(int size)
     for (int i = 0; i < size; i++)
     {
         cout << "__________";
+        
     }
+    
     cout << endl;
+
     for (int i = 0; i < size; i++)
     {
         cout << setw(10) << "|";
@@ -27,37 +34,43 @@ void printsep(int size)
 }
 void printUI()
 {
-    cout << "your direction= " << pg.direction << endl;
+    cout << "your direction= " << pg.playableworld.get_agent_direction() << endl;
     cout << "arrow left= " << pg.arrow << endl;
+    cout << "location= " << pg.playableworld.get_agent_location().first<<","<< pg.playableworld.get_agent_location().first << endl;
     for (int i = size - 1; i >= 0; i--)
     {
         printsep(size);
         for (int j = 0; j < size; j++)
         {
             string s = "";
-
+             bool here=(pg.playableworld.get_agent_location().first==i)&&(pg.playableworld.get_agent_location().second==j);
             if (pg.visiblity.at(i).at(j))
             {
+
                 if (pg.playableworld.world.boxes.at(i).at(j).get_breeze())
                 {
                     s = s + "B";
                 }
+
                 if (pg.playableworld.world.boxes.at(i).at(j).get_stench())
                 {
                     s = s + "S";
                 }
+
                 if (pg.playableworld.world.boxes.at(i).at(j).get_glitter())
                 {
                     s = s + "G";
                 }
-                if (pg.playableworld.world.boxes.at(i).at(j).has_gold())
+
+                if ( here)
                 {
-                    s = s + "Gold";
+                    s = s + "YOU";
                 }
                 if (pg.playableworld.world.boxes.at(i).at(j).has_pit())
                 {
                     s = s + "P";
                 }
+
                 if (pg.playableworld.world.boxes.at(i).at(j).has_wumpus())
                 {
                     s = s + "W";
@@ -71,7 +84,7 @@ void printUI()
 void ClearScreen()
 {
     int n;
-    for (n = 0; n < 10; n++)
+    for (n = 0; n < 1; n++)
         cout << "\n\n\n\n\n\n\n\n\n\n";
 }
 int main()
@@ -86,9 +99,10 @@ int main()
         cout << "not supoorted yet" << endl;
         return 1;
     }
-    playableWorld playableworld = playableWorld(make_pair(0, 0), "up",world);
-
+    playableWorld playableworld = playableWorld(make_pair(0, 0), "top", world);
+    //cout<<playableworld.world.boxes.at(0).at(0).get_breeze()<<endl;
     pg = PlayGround(player, playableworld);
+    //cout<<pg.playableworld.world.boxes.at(0).at(0).get_breeze()<<endl;
 
     for (int i = 0; i < size; i++)
     {
@@ -110,55 +124,62 @@ int main()
     cout<<endl<<endl<<i++<<endl;
     }
     */
+
     printUI();
     initscr();
     keypad(stdscr, TRUE);
-
+        // printUI();
     while (!pg.is_game_over())
-    {
-        vector<bool> arguments = {false, false, false, false};
-         vector<int> numOf_turns = {0, 0};
-        actions ac = actions(arguments.at(0),arguments.at(1), arguments.at(2), arguments.at(3), numOf_turns);
+    {   endwin();
+        printUI();
+ 
+        initscr();
+       keypad(stdscr, TRUE);  
+        ClearScreen();
 
-       
+        // printUI();
+        vector<bool> arguments = {false, false, false, false};
+        vector<int> numOf_turns = {0, 0};
+        actions ac = actions(arguments.at(0), arguments.at(1), arguments.at(2), arguments.at(3), numOf_turns);
+
         int ch = getch();
         switch (ch)
         {
 
         case KEY_BACKSPACE:
-            ClearScreen();
             cout << "game interupted by player" << endl;
             return 1;
             break;
 
         case KEY_LEFT:
-            ClearScreen();
 
             pg.changeDir("left");
-            ac.turnleft= true;
+            pg.playableworld.turn_left();
+            printw("/n",pg.playableworld.get_agent_direction());
+            ac.turnleft = true;
             numOf_turns.at(0)++;
 
             break;
 
         case KEY_RIGHT:
-            ClearScreen();
 
             pg.changeDir("right");
-            ac.turnright= true;
+            pg.playableworld.turn_right();
+            printw("/n",pg.playableworld.get_agent_direction());
+            ac.turnright = true;
             numOf_turns.at(1)++;
             break;
         case 10:
-            ClearScreen();
             ac.move = true;
-            pg.action=ac;
+            pg.action = ac;
+            cout<<"to infer"<<endl;
             pg.infer();
 
             break;
 
         case 's':
-            ClearScreen();
             ac.shoot = true;
-             pg.action=ac;
+            pg.action = ac;
             pg.infer();
             break;
         }
